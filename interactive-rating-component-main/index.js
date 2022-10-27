bdce61ca0edd4f14bf64333caa82b070572fbdfb
uk0724 <udaykumar.ab2017@gmail.com>
2022-10-27 10:11:19 +0530

    change button color

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -3,22 +3,15 @@ const submitButton = document.querySelector('.btn')
 const thanksContainer = document.querySelector('.thanks_container')
 const ratingContainer = document.querySelector('.rating-container')
 const rating = document.querySelector('.selected-rating')
-let selectedRating = 1
+let selectedRating = 0
 
 
 submitButton.addEventListener('click', () => {
-    thanksContainer.style.display = 'block'
-    ratingContainer.style.display = 'none'
-    // console.log(ratingContainer)
-    ratingButtons.forEach(button => {
-        let currentButton = button.classList.contains('active')
-        if (currentButton) {
-            selectedRating = button.innerText
-        }
-    });
-
-    rating.innerText = selectedRating
-
+    if (selectedRating > 0) {
+        thanksContainer.style.display = 'block'
+        ratingContainer.style.display = 'none'
+        rating.innerText = selectedRating
+    }
 })
 
 
@@ -26,8 +19,30 @@ ratingButtons.forEach(button => {
     button.addEventListener('click', () => {
         removeActiveClass()
         button.classList.add('active')
+        ratingButtons.forEach((button, index) => {
+            let currentButton = button.classList.contains('active')
+            if (currentButton) {
+                selectedRating = button.innerText
+            }
+        });
+        addGray(ratingButtons, selectedRating)
     })
 });
+
+
+function addGray(ratingButtons, selectedRating) {
+    for (let i = 0; i < ratingButtons.length; i++) {
+        console.log(ratingButtons[i])
+        if (i <= (selectedRating - 1)) {
+            ratingButtons[i].style.backgroundColor = 'hsl(216, 12%, 54%)'
+            ratingButtons[i].style.color = 'white'
+        }
+        else {
+            ratingButtons[i].style.backgroundColor = ''
+            ratingButtons[i].style.color = ''
+        }
+    }
+}
 
 
 const removeActiveClass = () => {
